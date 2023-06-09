16edc218ae334184c7733b8c93aab83e9473a032
safiislam <118913244+safiislam@users.noreply.github.com>
2023-06-09 08:51:49 +0600

    show user class data

--- src/pages/Dashbord/Instructor/MyClass/MyClass.jsx
+++ src/pages/Dashbord/Instructor/MyClass/MyClass.jsx
@@ -13,30 +13,37 @@ const MyClass = () => {
             return res.data
         }
     })
+    
     return (
         <div>
             <p className="text-center text-4xl font-bold mt-5" >My class</p>
             <div>
                 <div className="overflow-x-auto">
                     <table className="table">
-                        
+
                         <thead>
                             <tr>
-                                <th></th>
-                                <th>Name</th>
-                                <th>Job</th>
-                                <th>Favorite Color</th>
+                                <th>#</th>
+                                <th>Class Name</th>
+                                <th>Email</th>
+                                <th>Total Enroll</th>
+                                <th>Status</th>
+                                <th>action</th>
                             </tr>
                         </thead>
                         <tbody>
                             {/* row 1 */}
-                            <tr className="bg-base-200">
-                                <th>1</th>
-                                <td>Cy Ganderton</td>
-                                <td>Quality Control Specialist</td>
-                                <td>Blue</td>
-                            </tr>
-                            
+                            {
+                                instructorClass.map((item,index) => <tr key={item._id} className="bg-base-200">
+                                    <th>{index + 1 }</th>
+                                    <td>{item.courseName}</td>
+                                    <td>{item.instractorEmail}</td>
+                                    <td>{item.totalEnroll}</td>
+                                    <td>{item.status}</td>
+                                    <td><button onClick={()=>updateClass(item._id)} className="btn btn-secondary">Update</button></td>
+                                </tr>)
+                            }
+
                         </tbody>
                     </table>
                 </div>
